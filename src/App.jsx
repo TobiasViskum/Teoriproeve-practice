7af7804372cfcd93c7e659db50166afeda584ad8
TobiasViskum <tobias.t.viskum@gmail.com>
2023-08-04 11:35:24 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
     <>
       <main className="w-full pt-4 pb-12 px-4">
         <div className="pt-2 sm:pt-6 flex flex-col items-center gap-y-10">
-          <h1 className="text-center text-balance text-4xl sm:text-6xl font-bold">
+          <h1 className="text-center text-balance text-4xl md:text-6xl font-bold">
             {"Practice for the Teoriprøve!"}
           </h1>
           <Question />
         </div>
-        <p className="text-2xs fixed bottom-6 left-6">Version 1.1.2</p>
+        <p className="text-2xs fixed bottom-6 left-6">Version 1.1.3</p>
       </main>
     </>
   );
